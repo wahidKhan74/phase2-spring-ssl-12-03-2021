1732a011e21bc3b21cab569a9b2c3b541aac90a5
Wahid Khan <wahid.khan74@gmail.com>
2021-12-03 07:19:14 +0000

    add ssl to spring boot project

---
+++ src/main/java/com/ecom/webservice/Phase2SpringSsl12032021Application.java
@@ -0,0 +1,13 @@
+package com.ecom.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Phase2SpringSsl12032021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Phase2SpringSsl12032021Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecom/webservice/Phase2SpringSsl12032021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Phase2SpringSsl12032021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
